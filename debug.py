a17ad8aeb7b1916e8252b0a6f710a8142ede90ad
Xiao An <hac@zju.edu.cn>
2017-05-20 17:30:44 +0800

    fix bugs in custom level
    Signed-off-by: Xiao An <hac@zju.edu.cn>

--- debug.py
+++ debug.py
@@ -6,12 +6,15 @@ from cessa.config import Action, Level
 CLABEL_FILE='configs/clabel.json'
 RECORD_DIR='tmp'
 
-syscall_list = trace.data_preprocessing('tmp/nginx.trace.list', 'tmp')
+syscall_list = trace.data_preprocessing('tmp/nginx-test.trace', 'tmp')
 # rule_coll_list = rule.gen_rules(syscall_list, level=Level.NAME)
-# rule_coll_list = rule.gen_rules(syscall_list, record_dir='tmp', level=Level.ARG)
-rule_coll_list = rule.gen_rules(syscall_list, record_dir=RECORD_DIR, clabel_file=CLABEL_FILE, level=Level.CLABEL)
+#rule_coll_list = rule.gen_rules(syscall_list, record_dir='tmp', level=Level.ARG)
+#rule_coll_list = rule.gen_rules(syscall_list, record_dir=RECORD_DIR, clabel_file=CLABEL_FILE, level=Level.CLABEL)
+rule_coll_list = rule.gen_rules(syscall_list, record_dir=RECORD_DIR, clabel_file=CLABEL_FILE, level=Level.CUSTOM)
 print(len(rule_coll_list))
+#print(rule_coll_list)
 for r_c in rule_coll_list:
+    #print(r_c)
     print(r_c.name, r_c.level)
     for r in r_c.rules:
         print('\t', r)
