08a25fbc4154e89886a89556f060e1186996d589
肖安 <hac@zju.edu.cn>
2017-05-21 11:11:40 +0800

    updates syscall knowledge base for recvfrom(), against CVE-2013-2028
    Signed-off-by: 肖安 <hac@zju.edu.cn>

--- expert/compiled_krb/compiled_pyke_files.py
+++ expert/compiled_krb/compiled_pyke_files.py
@@ -13,20 +13,20 @@ except NameError:
 
 def get_target_pkg():
     return target_pkg.target_pkg(__name__, __file__, pyke_version, loader, {
-         ('', '', 'bc_relate.krb'):
-           [1495107420.334188, 'bc_relate_bc.py'],
-         ('', '', 'syscall.kfb'):
-           [1495107420.342045, 'syscall.fbc'],
-         ('', '', 'questions.kqb'):
-           [1495107420.347755, 'questions.qbc'],
-         ('', '', 'argument.kfb'):
-           [1495107420.349176, 'argument.fbc'],
          ('', '', 'macro.kfb'):
-           [1495107420.353362, 'macro.fbc'],
-         ('', '', 'question_fact.kfb'):
-           [1495107420.354085, 'question_fact.fbc'],
+           [1495335069.16785, 'macro.fbc'],
+         ('', '', 'syscall.kfb'):
+           [1495335069.183489, 'syscall.fbc'],
          ('', '', 'question_rule.krb'):
-           [1495107420.356874, 'question_rule_bc.py'],
+           [1495335069.190801, 'question_rule_bc.py'],
+         ('', '', 'argument.kfb'):
+           [1495335069.19258, 'argument.fbc'],
+         ('', '', 'questions.kqb'):
+           [1495335069.204699, 'questions.qbc'],
+         ('', '', 'question_fact.kfb'):
+           [1495335069.20759, 'question_fact.fbc'],
+         ('', '', 'bc_relate.krb'):
+           [1495335069.246141, 'bc_relate_bc.py'],
         },
         compiler_version)
 
